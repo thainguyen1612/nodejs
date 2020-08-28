eb496feefa37f04ff9991b680e8ad45c0fc13188
thainguyen1612 <nguyen_thai@teneocto.io>
2020-08-28 17:17:29 +0700

    mtp

--- src/module/post/postController.ts
+++ src/module/post/postController.ts
@@ -3,7 +3,6 @@ import { Request, Response } from 'express';
 type getPosts = Response;
 
 export function getPosts(req: Request, res: Response): Response {
-  const x: string = 'welcome';
 
   return res.send(x);
 }
